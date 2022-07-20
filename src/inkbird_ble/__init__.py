affd66cf1b2aa883408085286903fa78daade86e
J. Nick Koston <nick@koston.org>
2022-07-20 16:55:57 -0500

    feat: export SensorDescription and SensorValue

--- src/inkbird_ble/__init__.py
+++ src/inkbird_ble/__init__.py
@@ -2,7 +2,13 @@
 from __future__ import annotations
 
 from bluetooth_sensor_state_data import SIGNAL_STRENGTH_KEY
-from sensor_state_data import DeviceClass, DeviceKey, SensorUpdate
+from sensor_state_data import (
+    DeviceClass,
+    DeviceKey,
+    SensorDescription,
+    SensorUpdate,
+    SensorValue,
+)
 from sensor_state_data.data import (
     ATTR_HW_VERSION,
     ATTR_MANUFACTURER,
@@ -25,9 +31,11 @@ __all__ = [
     "ATTR_NAME",
     "ATTR_SW_VERSION",
     "SIGNAL_STRENGTH_KEY",
+    "SensorDescription",
     "SensorDeviceInfo",
     "DeviceClass",
     "DeviceKey",
     "SensorUpdate",
     "SensorDeviceInfo",
+    "SensorValue",
 ]
